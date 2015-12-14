5ffd4cc8f7835cd0a3a1cd33b6e61fd95cf445df
Martin Daguerre <mdaguerre@gmail.com>
2015-12-14 11:23:17 -0300

    Add express as dependenci and hello world route

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 var five = require("johnny-five");
 var Raspi = require("raspi-io");
+var express = require('express');
+var app = express();
+
 var board = new five.Board({
   io: new Raspi()
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
 });
 
 board.on("ready", function() {
